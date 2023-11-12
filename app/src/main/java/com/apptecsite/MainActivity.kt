8b7c978378f97e96c7b71e87696e8ed2152b41c3
BellidoJeremy <jeremy.bellido@tecsup.edu.pe>
2023-11-12 11:20:22 -0500

    Commit

--- app/src/main/java/com/apptecsite/MainActivity.kt
+++ app/src/main/java/com/apptecsite/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.apptecsite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun irAReservaPolideportivos(view: View) {
+        val intent = Intent(this, ReservaPolideportivosActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun irAReservaLaboratorios(view: View) {
+        val intent = Intent(this, ReservaLaboratoriosActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/apptecsite/ReservaPolideportivosActivity.kt
@@ -0,0 +1,12 @@
+package com.apptecsite
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReservaPolideportivosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reserva_polideportivos)
+
+    }
+}
